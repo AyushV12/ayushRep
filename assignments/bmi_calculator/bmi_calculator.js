aead0542e31bf0f373dbad4f91f5870d554c57ff
AYUSH <av5277670@gmail.com>
2021-11-28 23:33:02 +0530

    BMIASSIGNMENTCOMPLETED

--- assignments/bmi_calculator/bmi_calculator.js
+++ assignments/bmi_calculator/bmi_calculator.js
@@ -1,5 +1,6 @@
 // This function should return the BMI for a person 
-function BMICalculator(mass, height){
+function BMICalculator(mass, height) {
+    // Write your code here
     if(mass<=0  || height<=0){
         return("INVALID INPUT");
     }
