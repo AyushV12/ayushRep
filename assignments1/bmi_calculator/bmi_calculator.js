8afbf837ee54f6a1863860b0a9f258829af442fc
AYUSH <av5277670@gmail.com>
2021-11-29 17:11:33 +0530

    Create_Object_Assignment

---
+++ assignments1/bmi_calculator/bmi_calculator.js
@@ -0,0 +1,12 @@
+// This function should return the BMI for a person 
+function BMICalculator(mass, height){
+    if(mass<=0  || height<=0){
+        return("INVALID INPUT");
+    }
+    else{
+        var bmi=mass / (height * height);
+        return(bmi);
+    }
+}
+
+module.exports = BMICalculator;
